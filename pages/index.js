71d9247f039f0f19d005f63ddf344ca9a0a68e25
psych0sykes <blakesykes13@gmail.com>
2020-07-02 08:54:21 -0600

    Cleaned up state hook for patchConfig with patchChange function. Patch params are changed statefully and within a single state object.

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,20 @@ import {useState} from "react";
 
 export default function Home() {
 
-  const [patchConfig,setPatchConfig] = useState([{name: "volume",value: 10}])
+  const [patchConfig,setPatchConfig] = useState({})
+
+  const patchChange = (event)=> {
+      setPatchConfig({...patchConfig,[event.target.getAttribute("param")]: event.target.value})
+  }
 
   return (
     <div>
       <Section direction="column">
         <h1>synthesize_js</h1>
-        <input type="range" min="0" max="127" value={patchConfig[0].value} onChange={(event)=>handleChange(event)}/>
+        <input param="volume" type="range" min="0" max="127" value={patchConfig.volume} onChange={(event)=>patchChange(event)}/>
+        <input param="volume" type="range" min="0" max="127" value={patchConfig.volume} onChange={(event)=>patchChange(event)}/>
         <div>
-          {patchConfig[0].value}
+          {patchConfig.volume}
         </div>
       </Section>
     </div>
